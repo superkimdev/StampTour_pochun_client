845f12a6cd19a5ce8f0c75795260698a6c7662c8
Jellabean <mjkiol12@naver.com>
2016-10-12 23:23:13 +0900

    jellanbin 회원가입,아이디비번찾기 화면작업

---
+++ app/src/main/java/com/thatzit/kjw/stamptour_kyj_client/login/JoinActivity.java
@@ -0,0 +1,21 @@
+package com.thatzit.kjw.stamptour_kyj_client.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.thatzit.kjw.stamptour_kyj_client.R;
+
+public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_join);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
